3e25746dc48fa5f5d788d8a7c4ef0993eb968c62
Korobeynik <korobeynik77@gmail.com>
2019-01-13 12:40:09 +0200

    Mobile version

--- app/js/common.js
+++ app/js/common.js
@@ -1,30 +1,31 @@
-// Adds and removes body class depending on screen width.
-// function screenClass() {
-//     if($(window).innerWidth() > 960) {
-//         $('.service-grid').removeClass('owl-carousel');
-//     } else {
-//         $('.service-grid').addClass('owl-carousel');
-//             var owl2 = $('.service-grid');
-//             owl2.owlCarousel({
-//                 items:1,
-//                 loop:true,
-//                 margin: 0,
-//                 smartSpeed: 500,
-//                 //autoplay: true,
-//                 nav: true,
-//                 autoplayTimeout: 20000,
-//                 autoplayHoverPause: false
-//             });
-//     }
-// }
 
-// Fire.
-//screenClass();
+function screenClass() {
+    if($(window).innerWidth() > 960) {
+        $('.advantag-grid').removeClass('owl-carousel');
+    } else {
+        $('.advantag-grid').addClass('owl-carousel');
+            var owl2 = $('.advantag-grid');
+            owl2.owlCarousel({
+                items:1,
+                loop:true,
+                margin: 0,
+                smartSpeed: 500,
+                //autoplay: true,
+                //autoHeight:true,
+                nav: true,
+                autoplayTimeout: 20000,
+                autoplayHoverPause: false
+            });
+    }
+}
 
-// And recheck when window gets resized.
-// $(window).bind('resize',function(){
-//     screenClass();
-// });
+
+screenClass();
+
+
+$(window).bind('resize',function(){
+    screenClass();
+});
 
 $(function() {
 
